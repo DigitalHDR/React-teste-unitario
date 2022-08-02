59eb8789dd2aef66f173179b54cd96e34921d5de
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-08-02 13:44:59 -0300

    colocando tudo em um conjunto de test com describe

--- src/App.test.js
+++ src/App.test.js
@@ -1,47 +1,50 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 import App from './App'
 
-test('Deverá renderizar os elementos corretamente', () => {
-  render(<App />)
-  const texto = screen.queryByText('Hello,')
-  expect(texto).toBeInTheDocument()
-
-  const inputElement = screen.queryByPlaceholderText('digite seu nome')
-  expect(inputElement).toBeInTheDocument()
-
-  const imageElement = screen.queryByRole('img')
-  expect(imageElement).toBeInTheDocument()
-})
-
-test('devera encontrar a imagem com o texto alternativo correto', () => {
-  render(<App />)
-
-  const altTextImageElement = screen.queryByAltText('ilustração do pikachu')
-  expect(altTextImageElement).toBeInTheDocument()
-})
-
-test('deverá pegar o input corretamente', () => {
-  render(<App />)
-  const inputElement = screen.queryByPlaceholderText('digite seu nome')
-  fireEvent.change(inputElement, {
-    target: {
-      value: 'Lucas',
-    },
+describe('App', () => {
+  test('Deverá renderizar os elementos corretamente', () => {
+    render(<App />)
+    const texto = screen.queryByText('Hello,')
+    expect(texto).toBeInTheDocument()
+  
+    const inputElement = screen.queryByPlaceholderText('digite seu nome')
+    expect(inputElement).toBeInTheDocument()
+  
+    const imageElement = screen.queryByRole('img')
+    expect(imageElement).toBeInTheDocument()
   })
-
-  const valorDigitado = screen.queryByText('Lucas')
-  expect(valorDigitado).toHaveTextContent('Lucas')
-})
-
-test('deverá renderizar corretamente o texto digitado', () => {
-  render(<App />)
-
-  const inputElement = screen.queryByPlaceholderText('digite seu nome')
-  fireEvent.change(inputElement, {
-    target: {
-      value: 'Lucas',
-    },
+  
+  test('devera encontrar a imagem com o texto alternativo correto', () => {
+    render(<App />)
+  
+    const altTextImageElement = screen.queryByAltText('ilustração do pikachu')
+    expect(altTextImageElement).toBeInTheDocument()
   })
-  expect(screen.queryByText('Lucas')).toHaveTextContent('Lucas')
-  expect(screen.queryByText('Hello,')).toHaveTextContent('Hello, Lucas')
+  
+  test('deverá pegar o input corretamente', () => {
+    render(<App />)
+    const inputElement = screen.queryByPlaceholderText('digite seu nome')
+    fireEvent.change(inputElement, {
+      target: {
+        value: 'Lucas',
+      },
+    })
+  
+    const valorDigitado = screen.queryByText('Lucas')
+    expect(valorDigitado).toHaveTextContent('Lucas')
+  })
+  
+  test('deverá renderizar corretamente o texto digitado', () => {
+    render(<App />)
+  
+    const inputElement = screen.queryByPlaceholderText('digite seu nome')
+    fireEvent.change(inputElement, {
+      target: {
+        value: 'Lucas',
+      },
+    })
+    expect(screen.queryByText('Lucas')).toHaveTextContent('Lucas')
+    expect(screen.queryByText('Hello,')).toHaveTextContent('Hello, Lucas')
+  })
+  
 })
